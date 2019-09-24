f7e66047ee1c426210902a9506662fb7c13a180d
ankitsel9911 <55614015+ankitsel9911@users.noreply.github.com>
2019-09-24 09:51:14 +0530

    changeTestBase

--- src/main/java/com/spraxa/somatus/framwork/base/TestBase.java
+++ src/main/java/com/spraxa/somatus/framwork/base/TestBase.java
@@ -9,9 +9,6 @@ import java.util.Date;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 import com.google.common.io.Files;
-import com.relevantcodes.extentreports.ExtentReports;
-import com.relevantcodes.extentreports.ExtentTest;
-import com.relevantcodes.extentreports.LogStatus;
 
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
@@ -20,17 +17,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 
 import com.spraxa.somatus.framwork.util.TestUtil;
 
 public class TestBase {
 	public static WebDriver driver;
 	public static Properties prop;
-	public static ExtentReports extent;
-	public static ExtentTest logger;
 
 	public TestBase() {
 		try {
@@ -84,38 +76,5 @@ public class TestBase {
 	Files.copy(source, finalDestination);
 	return destination;
 }
-	
-	@AfterMethod
-	public void getResult(ITestResult result) throws Exception{
-		if(result.getStatus() == ITestResult.FAILURE){
-			logger.log(LogStatus.FAIL, "Test Case Failed is "+result.getName());
-			logger.log(LogStatus.FAIL, "Test Case Failed is "+result.getThrowable());
-			//To capture screenshot path and store the path of the screenshot in the string "screenshotPath"
-                        //We do pass the path captured by this mehtod in to the extent reports using "logger.addScreenCapture" method. 			
-                        String screenshotPath = getScreenhot(driver, result.getName());
-			//To add it in the extent report 
-			logger.log(LogStatus.FAIL, logger.addScreenCapture(screenshotPath));
-		}else if(result.getStatus() == ITestResult.SKIP){
-			logger.log(LogStatus.SKIP, "Test Case Skipped is "+result.getName());
-		}
-		// ending test
-		//endTest(logger) : It ends the current test and prepares to create HTML report
-		extent.endTest(logger);
-	}
-	
-
-	@AfterTest
-	public void endReport(){
-		// writing everything to document
-		//flush() - to write or update test information to your report. 
-                extent.flush();
-                //Call close() at the very end of your session to clear all resources. 
-                //If any of your test ended abruptly causing any side-affects (not all logs sent to ExtentReports, information missing), this method will ensure that the test is still appended to the report with a warning message.
-                //You should call close() only once, at the very end (in @AfterSuite for example) as it closes the underlying stream. 
-                //Once this method is called, calling any Extent method will throw an error.
-                //close() - To close all the operation
-                extent.close();
-    }
-	
 
 }
