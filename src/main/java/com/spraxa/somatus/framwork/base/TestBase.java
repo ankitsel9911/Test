ce39335e0d0feff48709da4bdfee33d81bff59ff
ankitsel9911 <55614015+ankitsel9911@users.noreply.github.com>
2019-09-24 08:35:01 +0530

    resizeUpdate

--- src/main/java/com/spraxa/somatus/framwork/base/TestBase.java
+++ src/main/java/com/spraxa/somatus/framwork/base/TestBase.java
@@ -56,7 +56,7 @@ public class TestBase {
 			driver = new InternetExplorerDriver();
 		}
 
-		driver.manage().window().setSize(new Dimension(1382, 754));
+		driver.manage().window().setSize(new Dimension(1900, 1200));
 		driver.manage().deleteAllCookies();
 		//driver.manage().window().maximize();
 		driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
