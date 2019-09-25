11b0c18f65cafc3d84ae168df89471e0f7f6f526
ankitsel9911 <55614015+ankitsel9911@users.noreply.github.com>
2019-09-25 11:40:52 +0530

    zz

--- src/main/java/com/spraxa/somatus/framwork/base/TestBase.java
+++ src/main/java/com/spraxa/somatus/framwork/base/TestBase.java
@@ -52,7 +52,7 @@ public class TestBase {
 			driver = new InternetExplorerDriver();
 		}
 
-		driver.manage().window().setSize(new Dimension(1936,1056));
+		driver.manage().window().setSize(new Dimension(2560,1440));
 		driver.manage().deleteAllCookies();
 		//driver.manage().window().maximize();
 		driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
