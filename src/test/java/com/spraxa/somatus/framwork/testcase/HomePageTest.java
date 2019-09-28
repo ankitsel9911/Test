247f92b6cfac1a155afa394678ae0107e4c2b739
ankitsel9911 <55614015+ankitsel9911@users.noreply.github.com>
2019-09-28 15:43:07 +0530

    ff

--- src/test/java/com/spraxa/somatus/framwork/testcase/HomePageTest.java
+++ src/test/java/com/spraxa/somatus/framwork/testcase/HomePageTest.java
@@ -46,7 +46,7 @@ public class HomePageTest extends TestBase {
 		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
 	}
 
-	@Test
+	//@Test
 	public void tc003() throws Exception {
 		logger = extent.startTest(prop.getProperty("tc02StartTest"));
 		logger.log(LogStatus.PASS, prop.getProperty("logSuccessful"));
